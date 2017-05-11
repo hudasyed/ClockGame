3bd36cd494713f93d008e7f393309edefb59f1ae
HudaSyed <who.the.syed@gmail.com>
2017-05-10 23:36:57 -0700

    Setup Level 1
    Able to generate random hours and half hours for lowest common core
    compliance.

--- clock-game/src/app/app.component.ts
+++ clock-game/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  today: number = Date.now();
+  guess: number = this.getRandom();
+
+  getRandom()
+  {
+    const randHour =  Math.floor(Math.random() * (24 - 1) + 1);
+    const randTime = Math.random() >= 0.5;
+    const minutes = randTime ? 0 : 30;
+
+    return new Date().setHours(randHour, minutes , 0);
+  }
 }
