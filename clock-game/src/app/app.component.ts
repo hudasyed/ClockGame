14901c3edd2730ff62400d991a41e8e1714404c3
HudaSyed <who.the.syed@gmail.com>
2017-05-15 19:43:36 -0700

    Updating Clock Components

--- clock-game/src/app/app.component.ts
+++ clock-game/src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  today: number = Date.now();
-  guess: number = this.getRandom();
+export class AppComponent{
+  guess: number = AppComponent.getRandom();
+  clock = Observable
+    .interval(1000)
+    .map(()=> new Date());
 
-  getRandom()
-  {
+
+
+  static getRandom() {
     const randHour =  Math.floor(Math.random() * (24 - 1) + 1);
     const randTime = Math.random() >= 0.5;
     const minutes = randTime ? 0 : 30;
-
     return new Date().setHours(randHour, minutes , 0);
   }
 }
